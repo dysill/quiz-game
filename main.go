c6687544851dda5fa7ad9139aae12bad09b9cce6
dysill <dylanmsill@gmail.com>
2026-06-22 19:47:36 -0700

    read csv

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello quiz game")
+	// handle flags for time and file (later)
+	inputCsv := "problems.csv"
+
+	// Read the CSV
+	file, err := os.Open(inputCsv)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(file)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(records)
+	fmt.Println("Welcome to the quiz! You have _ seconds to answer the following _ questions:")
+
 }
