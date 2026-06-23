abac38ae4e87f48412fd8ed29a5f9713db0b2516
dysill <dylanmsill@gmail.com>
2026-06-22 19:57:02 -0700

    basic quiz implementation

--- main.go
+++ main.go
@@ -11,19 +11,28 @@ func main() {
 	// handle flags for time and file (later)
 	inputCsv := "problems.csv"
 
-	// Read the CSV
 	file, err := os.Open(inputCsv)
 	if err != nil {
 		log.Fatal(err)
 	}
 	r := csv.NewReader(file)
-
 	records, err := r.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(records)
-	fmt.Println("Welcome to the quiz! You have _ seconds to answer the following _ questions:")
+	numQuestions := len(records)
+	var userAnswer string
+	var correctAnswers int
+
+	fmt.Printf("Welcome to the quiz! You have _ seconds to answer the following %d questions:\n", numQuestions)
+	for i, question := range records {
+		fmt.Printf("Problem %d: %s = ", i, question[0])
+		fmt.Scan(&userAnswer)
+		if userAnswer == question[1] {
+			correctAnswers++
+		}
+	}
+	fmt.Printf("You scored %d out of %d.\n", correctAnswers, numQuestions)
 
 }
